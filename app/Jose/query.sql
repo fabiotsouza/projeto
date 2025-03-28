f5587dad8799430cb7e9bdb05b9d7bd236b36a14
joseAmatto <amattonetto@gmail.com>
2025-03-28 14:38:59 -0300

    Update query.sql

--- app/Jose/query.sql
+++ app/Jose/query.sql
@@ -1,3 +1,19 @@
-SELECT * FROM cortes WHERE nome like 'De%';
+SELECT * FROM cortes;
 
-INSERT INTO cortes (nome, preco) VALUE ('Degrade', 35.00);
+SELECT * FROM cortes WHERE nome LIKE 'De%';
+SELECT * FROM cortes WHERE nome LIKE 'Un%';
+SELECT * FROM cortes WHERE nome LIKE 'Bl%';
+SELECT * FROM cortes WHERE nome LIKE 'Bu%';
+SELECT * FROM cortes WHERE nome LIKE 'Po%';
+
+
+SELECT * FROM cortes WHERE preco >= 20;
+SELECT * FROM cortes WHERE preco <= 120;
+
+REMOVE FROM cortes WHERE id = 1;
+
+INSERT INTO cortes (nome, preco, imagem) VALUES ('Degrade', 35, 'https://via.placeholder.com/80');
+INSERT INTO cortes (nome, preco, imagem) VALUES ('Undercut', 45.00, 'https://via.placeholder.com/80');
+INSERT INTO cortes (nome, preco, imagem) VALUES ('Black Power',  50.00, 'https://via.placeholder.com/80');
+INSERT INTO cortes (nome, preco, imagem) VALUES ('Buzzed', 60.00, 'https://via.placeholder.com/80');
+INSERT INTO cortes (nome, preco, imagem) VALUES ('Pompadour', 120.00, 'https://via.placeholder.com/80');
