984ebbe5587ddac48fb49f71791b42efb4688b8b
joseAmatto <amattonetto@gmail.com>
2025-03-28 15:18:16 -0300

    Update query.sql

--- app/Jose/query.sql
+++ app/Jose/query.sql
@@ -1,20 +1,34 @@
+-- atualizar dados da tela cortes
+
+UPDATE cortes SET preco = 80 WHERE id = 5;
+
+
+
+-- selecionar tudo da pagina de cortes
+
 SELECT * FROM cortes;
 
-SELECT * FROM cortes WHERE nome LIKE 'De%';
-SELECT * FROM cortes WHERE nome LIKE 'Un%';
-SELECT * FROM cortes WHERE nome LIKE 'Bl%';
-SELECT * FROM cortes WHERE nome LIKE 'Bu%';
-SELECT * FROM cortes WHERE nome LIKE 'Po%';
+
+-- deletar tudo de um corte 
+
+DELETE * FROM cortes WHERE nome = 'Degrade'
 
 
-SELECT * FROM cortes WHERE preco >= 20 AND preco <= 50;
-SELECT * FROM cortes WHERE preco >= 60;
+
+
+-- remover cortes da lista algo especifico
 
 REMOVE FROM cortes WHERE id = 1;
+
+
+
+-- inserir cortes na lista
+
 
 INSERT INTO cortes (nome, preco, imagem) VALUES ('Degrade', 35, 'https://via.placeholder.com/80');
 INSERT INTO cortes (nome, preco, imagem) VALUES ('Undercut', 45.00, 'https://via.placeholder.com/80');
 INSERT INTO cortes (nome, preco, imagem) VALUES ('Black Power',  50.00, 'https://via.placeholder.com/80');
 INSERT INTO cortes (nome, preco, imagem) VALUES ('Buzzed', 60.00, 'https://via.placeholder.com/80');
-INSERT INTO cortes (nome, preco, imagem) VALUES ('Pompadour', 120.00, 'https://via.placeholder.com/80');
+INSERT INTO cortes (nome, preco, imagem) VALUES ('Pompadour', 80.00, 'https://via.placeholder.com/80');
+INSERT INTO cortes (nome, preco, imagem) VALUES ('luzes', 120.00, 'https://via.placeholder.com/80');
 
