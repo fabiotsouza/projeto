e363f27a6cb316c8e632bc2659cba0e0f3b50ed8
Gabriel-Porcel <2gabrielporcel@gmail.com>
2025-04-08 14:05:12 -0300

    lib

---
+++ app/lib/conexao.js
@@ -0,0 +1,13 @@
+import mysql from "mysql2/promise";
+
+const conexao = await mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "projeto"
+    }
+)
+
+
+export default conexao;
