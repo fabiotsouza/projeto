47358249fcc26b499eed2c93a319e908897e6239
tunes-gabriel <gabrie.tunes@gmail.com>
2025-04-24 17:20:20 -0300

    menu superior pronto

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -2,7 +2,7 @@
 
 import "./menu.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faHouse, faScissors, faFile } from "@fortawesome/free-solid-svg-icons"
+import { faBars, faHouse, faScissors, faFile, faDoorClosed } from "@fortawesome/free-solid-svg-icons"
 import { useState } from "react";
 import { redirect } from "next/navigation";
 
@@ -10,6 +10,10 @@ import { redirect } from "next/navigation";
 function Menu() {
     const [open, setOpen] = useState(false);
     
+    function deslogar(){
+
+        localStorage.removeItem("usuario")
+    }
 
     return (
         <div className="menuSup">
@@ -22,10 +26,7 @@ function Menu() {
                     
                 <div className="dropdownMenu">
                     <ul>
-                        <DropdownItem  icon={faHouse} text={" Home"} onClick={()=> window.location.href("./")}/>
-                        <DropdownItem icon={faScissors} text={" Cortes"} onClick={()=> window.location.href("../Jose")}/>
-                        <DropdownItem icon={faFile} text={" Relatório"}/>
-
+                        {DropdownItem()}
                     </ul>
 
                 </div>
@@ -35,14 +36,26 @@ function Menu() {
             <div></div>
         </div>
     );
-}
-function DropdownItem(e){
-    return(
-        <li className="dropdownItem">
-            <FontAwesomeIcon icon={e.icon} />
-            <a>{e.text}</a>
-        </li>
-    )
+    function DropdownItem(){
+        return(
+            <li className="dropdownItem">
+                <div>
+                    <button onClick={()=> window.location.href="./home_barbeiro"}><FontAwesomeIcon icon={faHouse} /></button>
+                    <a onClick={()=> window.location.href="./home_barbeiro"}>Home</a>
+                </div>
+                <br/>
+                <div>
+                    <button onClick={()=> window.location.href="../Jose"}><FontAwesomeIcon icon={faScissors}/></button>
+                    <a onClick={()=> window.location.href="../Jose"}>Cortes</a>
+                </div>
+                <br/>
+                <div>
+                    <button  onClick={()=> {deslogar(), window.location.href = "./"}}><FontAwesomeIcon icon={faDoorClosed}/></button>
+                    <a>Deslogar</a>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default Menu;
