525443ac22ffc043a1f9affb53e73cec4024657b
Gabriel-Porcel <2gabrielporcel@gmail.com>
2025-04-24 14:57:29 -0300

    deslogar v1

--- app/components/Menu_cliente.js
+++ app/components/Menu_cliente.js
@@ -1,11 +1,18 @@
 import "./Menu_cliente.css"
 
 function Menu_cliente() {
+
+    function deslogar(){
+
+        localStorage.removeItem("usuario")
+    }
+
     return ( 
 
         <div className="menuSup">
             <button className="voltar" onClick={()=> window.location.href = "/home_cliente"}>Voltar</button>
             <img src="https://placehold.co/100x40" />
+            <button onClick={()=> {deslogar(), window.location.href = "./"}}>Deslogar</button>
         </div>
 
     );
