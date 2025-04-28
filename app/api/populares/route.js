4a53f8c3c36f8131932634bd45ef4d59cdc245a0
Gabriel-Porcel <2gabrielporcel@gmail.com>
2025-04-28 13:52:40 -0300

    populares teste

--- app/api/populares/route.js
+++ app/api/populares/route.js
@@ -3,9 +3,18 @@ import conexao from "@/app/lib/conexao";
 export async function GET(){
 
     const query = `
-        SELECT c.nome AS cortes, COUNT(a.id) AS total_realizados
-        FROM agendamentos a JOIN cortes c ON a.id_corte = c.id
-        GROUP BY c.nome ORDER BY total_realizados DESC LIMIT 5; 
+        SELECT 
+        c.nome AS corte,
+        COUNT(*) AS total_agendamentos
+        FROM 
+        agendamentos a
+        JOIN 
+        cortes c ON a.corte_id = c.id
+        GROUP BY 
+        a.corte_id, c.nome
+        ORDER BY 
+        total_agendamentos DESC
+        LIMIT 10;
     `
 
     const [results] = await conexao.execute(query)
